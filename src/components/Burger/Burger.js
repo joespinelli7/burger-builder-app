2ef2c95046c40d0698049ae69d014bf9b008483a
github username <josephspinelli@yahoo.com>
2019-05-12 15:40:11 -0400

    burger adjustments

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -6,13 +6,19 @@ const burger = (props) => {
   // Object.keys is a default JS object which has a keys method that extracts extracts keys from given
   // object and returns an array of those keys. Doing this to take state of BurgerBuilder (which is an object)
   // and return an array of subarrays with length of each ingredient array which equals total # of that ingredient.
-  const arrayIngredients = Object.keys(props.ingredients)
+  let arrayIngredients = Object.keys(props.ingredients)
     .map(ingredientKey => {
       return [...Array(props.ingredients[ingredientKey])].map((_, i) => {
         return <BurgerIngredient key={ingredientKey + i} type={ingredientKey} />;
       })
-    });
+    })
+    .reduce((arr, el) => {
+      return arr.concat(el)
+    }, []);
 
+  if(arrayIngredients.length === 0) {
+    arrayIngredients = <p>Please add ingredients to burger.</p>
+  }
   return(
     <div className={classes.Burger}>
       <BurgerIngredient type="bread-top" />
