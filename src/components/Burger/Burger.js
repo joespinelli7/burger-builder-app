74c0685f357d4ab6be5a5e779ca5555642e612b3
github username <josephspinelli@yahoo.com>
2019-05-12 01:50:21 -0400

    outputting burger ingredients depending on state

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,11 +3,20 @@ import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient'
 
 const burger = (props) => {
+  // Object.keys is a default JS object which has a keys method that extracts extracts keys from given
+  // object and returns an array of those keys. Doing this to take state of BurgerBuilder (which is an object)
+  // and return all the states key and value pairs
+  const arrayIngredients = Object.keys(props.ingredients)
+    .map(igKey => {
+      return [...Array(props.ingredients[igKey])].map((_, i) => {
+        return <BurgerIngredient key={igKey + i} type={igKey} />;
+      })
+    });
+
   return(
     <div className={classes.Burger}>
       <BurgerIngredient type="bread-top" />
-      <BurgerIngredient type="cheese" />
-      <BurgerIngredient type="meat" />
+      {arrayIngredients}
       <BurgerIngredient type="bread-bottom" />
     </div>
   );
