08e83725799b8958afd47841bf4742480c399b50
github username <josephspinelli@yahoo.com>
2019-06-07 01:12:54 -0400

    reusing backdrop for mobile and improving side drawer

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -4,14 +4,31 @@ import classes from './Layout.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
-const layout = (props) => (
-  <Aux>
-    <Toolbar />
-    <SideDrawer />
-    <main className={classes.Content}>
-      {props.children}
-    </main>
-  </Aux>
-);
+// turn this component into a class component b/c this is where we use both toolbar and sidedrawer
+// so we can implement method that can listen to both sideDrawer closing by clicking on the Backdrop
+// as well as the toolbar opening the sideDrawer by clicking on the toggle btn.
+class Layout extends React.Component {
+  state = {
+    showSideDrawer: true
+  }
 
-export default layout;
+  sideDrawerClosedHandler = () => {
+    this.setState({
+      showSideDrawer: false
+    })
+  }
+
+  render() {
+    return (
+      <Aux>
+        <Toolbar />
+        <SideDrawer open={this.state.showSideDrawer} closed={this.sideDrawerClosedHandler}/>
+        <main className={classes.Content}>
+          {this.props.children}
+        </main>
+      </Aux>
+    )
+  }
+}
+
+export default Layout;
